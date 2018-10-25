0edb67e05e609fdcbe1443cf6b0f1784857d16c5
jerrychir <jerrychir@163.com>
2018-10-25 12:56:16 +0800

    pom config

---
+++ discovery/src/main/java/com/sangoes/cloud/discovery/DiscoveryApplication.java
@@ -0,0 +1,19 @@
+package com.sangoes.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Copyright (c) 2018
+ *
+ * @author jerrychir
+ * @date 2018/10/25 12:28 PM
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
