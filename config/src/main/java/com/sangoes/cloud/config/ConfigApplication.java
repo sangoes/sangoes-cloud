2a6cefbf8d18c66d06010f89ceec19d1cddca9f6
Jerry Chir <jerrychir@gmail.com>
2019-02-20 11:23:21 +0800

    add config

--- config/src/main/java/com/sangoes/cloud/config/ConfigApplication.java
+++ config/src/main/java/com/sangoes/cloud/config/ConfigApplication.java
@@ -1,18 +1,18 @@
 package com.sangoes.cloud.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Copyright (c) 2018
+ * 配置中心
  *
  * @author jerrychir
  * @date 2018/10/25 2:09 PM
  */
-//@EnableDiscoveryClient
 @EnableConfigServer
-@SpringBootApplication
+@SpringCloudApplication
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
